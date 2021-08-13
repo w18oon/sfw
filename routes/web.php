b3e17ce7356d4c3b350ec73d078cb8d4a29adb3b
Wiboon Supachitranant <49589301+w18oon@users.noreply.github.com>
2021-08-13 23:15:56 +0700

    add storage-link

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MemberController;
 use App\Models\Postcode;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/register-form', function () {
     return view('register-form',[
@@ -12,12 +13,15 @@ Route::get('/register-form', function () {
 
 Route::get('/', function() {
     return view('home',[]);
-    // echo resource_path();
 });
 Route::get('/search', [App\Http\Controllers\SearchController::class, 'index']);
 Route::post('/search', [App\Http\Controllers\SearchController::class, 'index']);
 Route::get('/receipt/{id}', [App\Http\Controllers\ReceiptController::class, 'show']);
 Route::get('/contract/{id}', App\Http\Controllers\ContractController::class);
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+});
 
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/member', [MemberController::class, 'index'])->name('member.index');
